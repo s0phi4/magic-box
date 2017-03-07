b9356cb8175ee32ae9cc46e000e5bc5f5653cb5a
ddobson <ddobson445@gmail.com>
2017-03-07 18:48:54 -0500

    Initial Commit
    Initialize repository using:
    - GA browser tempalte
    - CT-Paper Bootstrap Theme
    - Font Awesome Styles & Fonts
    - Lodash

---
+++ assets/scripts/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const config = {
+  apiOrigins: {
+    production: 'https://ga-wdi-boston.herokuapp.com',
+    development: 'http://localhost:4741'
+  },
+};
+
+module.exports = config;
